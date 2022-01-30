67af4f437253a53f6bd90360314de2a196ec6efe
Serhii Varakuta <flood.code@gmail.com>
2022-01-30 20:01:58 +0200

    Implement pool job processor

--- go.mod
+++ go.mod
@@ -14,7 +14,9 @@ require (
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.1.5 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.9 // indirect
 	github.com/tklauser/numcpus v0.3.0 // indirect
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -3,11 +3,21 @@ package bot
 import (
 	"context"
 	"fmt"
+	"math/big"
+	"sync"
+	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 
+	"github.com/bcnmy/hyphen-arbitrage/internal/erc20"
 	"github.com/bcnmy/hyphen-arbitrage/internal/pool"
 )
+
+const connectionTimeout = time.Second * 15
+const jobTimeout = time.Second * 15
 
 type Bot interface {
 	// Starts bot worker
@@ -24,20 +34,105 @@ func New(mgr pool.Manager, logger log.Logger) (Bot, error) {
 			logger,
 			"mgr", fmt.Sprintf("%s:%s:%s", mgr.PoolName(), mgr.AccountName(), mgr.TokenName()),
 		),
+
+		running: make(chan bool),
 	}, nil
 }
 
 type bot struct {
 	mgr    pool.Manager
 	logger log.Logger
+
+	running chan bool
+	poolWg  sync.WaitGroup
 }
 
 func (b *bot) Run(ctx context.Context) error {
+	for _, p := range b.mgr.Pools() {
+		go b.processPool(ctx, p)
+	}
+
 	<-ctx.Done()
+	close(b.running)
+	b.poolWg.Wait()
+
 	return nil
 }
 
 func (b *bot) Stop(ctx context.Context) error {
 	<-ctx.Done()
 	return nil
+}
+
+func (b *bot) processPool(ctx context.Context, p pool.Pool) {
+	level.Info(b.logger).Log("network", p.NetworkName(), "msg", "starting processing pool")
+
+	b.poolWg.Add(1)
+	defer b.poolWg.Done()
+
+	dialCtx, cancel := context.WithTimeout(ctx, connectionTimeout)
+	defer cancel()
+
+	client, err := p.Dial(dialCtx)
+	if err != nil {
+		level.Error(b.logger).Log("msg", "unable to create EVM client for pool", "err", err)
+		return
+	}
+
+	jobs := make(chan bool, 4)
+	jobs <- true
+
+	for {
+		select {
+		case _, closed := <-b.running:
+			if closed {
+				return
+			}
+		case <-jobs:
+			jobCtx, cancel := context.WithTimeout(ctx, jobTimeout)
+			defer cancel()
+
+			err = b.processPoolJob(jobCtx, client, p)
+			if err != nil {
+				level.Error(b.logger).Log("msg", "unable to process pool job", "err", err)
+				return
+			}
+		}
+	}
+}
+
+func (b *bot) processPoolJob(ctx context.Context, client *ethclient.Client, p pool.Pool) error {
+	level.Info(b.logger).Log("network", p.NetworkName(), "msg", "processing new pool job")
+	erc20Contract, err := erc20.NewErc20(p.Token(), client)
+	if err != nil {
+		return err
+	}
+
+	balance, err := erc20Contract.BalanceOf(&bind.CallOpts{
+		Context: ctx,
+	}, b.mgr.AccountAddress())
+
+	if err != nil {
+		return err
+	}
+
+	level.Debug(b.logger).Log("network", p.NetworkName(), "msg", fmt.Sprintf("token balance: %s", balance.String()))
+
+	_, err = b.calculateProfitability(client, p, *balance)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *bot) calculateProfitability(client *ethclient.Client, p pool.Pool, maxDeposit big.Int) (*profitability, error) {
+	// TODO: Implementation
+	return &profitability{}, nil
+}
+
+type profitability struct {
+	deposit  big.Int
+	reward   big.Int
+	gasPrice big.Int
 }
